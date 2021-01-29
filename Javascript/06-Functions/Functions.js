ab0520663370ac680a1c26a2ca939abd32c9cc85
DanielQA123 <DAhuchogu@QA.com>
2021-01-29 02:07:28 +0000

    DA: working on functions

---
+++ Javascript/06-Functions/Functions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+add(12,3); // creating and hosting a function at the top of the file
+
+//function decleration = HOISTS
+function add(){
+    console.log(1+2);
+}
+
+// hello(); no allowed! 
+
+
+
