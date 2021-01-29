eb6c7a784919fc07e5f5f750836c7812199998b7
DanielQA123 <DAhuchogu@QA.com>
2021-01-29 01:52:04 +0000

    DA: Javascript scope

--- Javascript/04-Scope/Scope.js
+++ Javascript/04-Scope/Scope.js
@@ -1,1 +1,20 @@
-'use strict'
+//'use strict'
+//With scope you don't really need USE STRICT term but
+// you generally want to have it when working on JS
+
+function deposit(increment){
+    bankbalance = 1000;
+    let balance = bankbalance + increment;
+    return `${balance}`;
+}
+
+console.log(deposit(10));
+console.log(bankbalance);
+
+var x =10;
+console.log(x + "" + y);
+var y = 12;
+
+let x2 = 10;
+console.log(x2 + "" + y2);
+let y2 = 15;
