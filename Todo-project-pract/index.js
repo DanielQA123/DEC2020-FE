81e467a60137139a394e369592dee99cab4eee9c
DanielQA123 <DAhuchogu@QA.com>
2021-02-13 21:30:54 +0000

    DA: Made changes

--- Todo-project-pract/index.js
+++ Todo-project-pract/index.js
@@ -3,6 +3,13 @@
 const todoURL = "http://localhost:8080/todo";
 const todoSystemURL = "http://localhost:8080/todoSystem";
 
+const todoTitle = document.querySelector('#createTitle');
+const todoDescription = document.querySelector('#createDescription');
+const todoDate = document.querySelector('#createDate');
+const todoFinish = document.querySelector('#createFinish')
+
+const todoIdDelete = document.querySelector("#todoIdDelete");
+
 //CRUD FUNCTIONALITY => CREATE, READ, UPDATE, DELETE
 //API => POST, GET, PUT, DELETE
 
@@ -10,7 +17,27 @@ const todoSystemURL = "http://localhost:8080/todoSystem";
 
 //POST = CREATE:
 const createTodo = () => {
-    fetch()
+
+    const todoTitleValue = todoTitle.value;
+    const todoDescriptionValue = todoDescription.value;
+    const todoDateValue = todoDate.value;
+    const todoFinishValue = todoFinish.value;
+
+    let data = {
+
+        "Title":todoTitleValue,
+        "Description":todoDescriptionValue,
+        "Date":todoDateValue,
+        "Finish":todoFinishValue
+    }
+
+    fetch("http://localhost:8080/todo/create", {
+        
+    })
+
+
+
+
 }
 
 
@@ -18,15 +45,15 @@ const createTodo = () => {
 
 const readTodoId = () => {
     fetch('http://localhost:8080/todo/1')
-    .then((response) =>{
-        if(response.status !== 200){
-            console.log(`Looks Like there was a problem.Status Code: ${response.status}`);
-            return;
-        }
-        response.json().then((data) => console.log(data));
-    })
-    .catch((err) => console.log(`Fetch Error: -S ${err}`)
-    );
+        .then((response) => {
+            if (response.status !== 200) {
+                console.log(`Looks Like there was a problem.Status Code: ${response.status}`);
+                return;
+            }
+            response.json().then((data) => console.log(data));
+        })
+        .catch((err) => console.log(`Fetch Error: -S ${err}`)
+        );
 
 }
 
@@ -34,13 +61,13 @@ const readTodoId = () => {
 
 const readAllTodo = () => {
     fetch('http://localhost:8080/todo/readAll')
-    .then(response => response.json())
-    .then(info =>{
-        for(let band of info){
-            console.log(band);
-        }
-    })
-    .catch(err => console.error(`error ${err}`))
+        .then(response => response.json())
+        .then(info => {
+            for (let band of info) {
+                console.log(band);
+            }
+        })
+        .catch(err => console.error(`error ${err}`))
 }
 
 //PUT = UPDATE:
@@ -52,14 +79,16 @@ const updateTodo = () => {
 //DELETE/REMOVE = DELETE: 
 const deleteTodo = () => {
 
-    fetch(`http://localhost:8080/todo/delete/${todoIdDelete}`,{
-        method:"DELETE",
-        headers:{
-            "Content-Type":"application/json"
+    const todoIdDelete = _todoIdDelete.value;
+
+    fetch(`http://localhost:8080/todo/delete/${todoIdDelete}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json"
         }
     })
-    .then(response => response.json())
-    .then(info => console.log(info))
-    .catch(err => console.error(`Something Went Wrong!! ${err}`))
+        .then(response => response.json())
+        .then(info => console.log(info))
+        .catch(err => console.error(`Something Went Wrong!! ${err}`))
 
 }
